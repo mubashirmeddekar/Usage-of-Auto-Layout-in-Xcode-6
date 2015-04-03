b32c793fe318f4174eb5c2bb0aec5904e64c9f20
Mubashir Meddekar <mubashirmeddekar@yahoo.com>
2015-04-03 17:49:17 +0530

    Usage of Auto Layout in Xcode 6
    Auto Layout to support multiple iOS Device Screens

---
+++ SampleMultiscreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleMultiscreen
+//
+//  Created by Mubashir Meddekar on 24/03/15.
+//  Copyright (c) 2015 Mubashir Meddekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleMultiscreen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleMultiscreen
+//
+//  Created by Mubashir Meddekar on 24/03/15.
+//  Copyright (c) 2015 Mubashir Meddekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
